bb4d09e4ecbd65f9175b0e5926f8b0bcf2d7a925
marvmilo <marvin.milojevic@gmail.com>
2021-12-14 14:34:43 +0100

    saved files from raspberry pi

--- cam/__init__.py
+++ cam/__init__.py
@@ -1,8 +1,37 @@
 import threading
+import time
+import os
+
+#import other scripts
+from . import hough
+
+#files
+image = "./cam/image.jpg"
+location_image = "./cam/location_image.jpg"
 
 #main function of script
 def Main(values, sql):
-    print("this is cam Script")
+    while True:
+        #capture picture
+        os.system(f"libcamera-jpeg -o {image} -t 1 --width 1014 --height 760 > /dev/null 2>&1")
+        
+        #get circles through hought transformation
+        circles = hough.transformation(image)
+        
+        if circles is not None:
+            x, y, color = hough.cordinates(circles, image)
+            hough.save_location_image(circles, image, location_image)
+            
+            values.ball.x = x
+            values.ball.y = y
+            values.ball.color = color
+        else:
+            values.movmentclear = False
+            values.ball.x = 0
+            values.ball.y = 0
+            values.ball.color = None
+        
+        time.sleep(1)
     
 #main thread of script
 class Thread(threading.Thread):
---
+++ cam/hough.py
@@ -0,0 +1,72 @@
+import cv2
+
+#hough transformation for finding circlles
+def transformation(filename):
+    # Loads an image
+    src = cv2.imread(cv2.samples.findFile(filename), cv2.IMREAD_COLOR)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        return -1
+    
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+    
+    rows = gray.shape[0]
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=1, maxRadius=30)
+    
+    return circles
+
+#get cordinates from circles
+def cordinates(circles, filename):
+    circle = circles[0][0]
+    src = cv2.imread(cv2.samples.findFile(filename), cv2.IMREAD_COLOR)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        return -1
+    
+    center = (circle[0], circle[1])
+    color = (src[int(circle[1]), int(circle[0]+10)])
+
+    color_string = None
+    if color[0] < 15:
+        color_string = "yellow"
+    elif color[1] < 100:
+        color_string = "red"
+    else:
+        color_string = "green"
+    
+    cal1_px = (229, 130)
+    cal2_px = (962, 622)
+    x_px_total = cal2_px[1] - cal1_px[1]
+    y_px_total = cal2_px[0] - cal1_px[0]
+    x_mm_total = 207
+    y_mm_total = 305
+    
+    pos_px_x = x_px_total - (circle[1] - cal1_px[1])
+    pos_px_y = y_px_total - (circle[0] - cal1_px[0])
+    
+    pos_perc_x = pos_px_x / x_px_total
+    pos_perc_y = pos_px_y / y_px_total
+    
+    pos_mm_x = x_mm_total * pos_perc_x
+    pos_mm_y = y_mm_total * pos_perc_y
+    
+    return int(pos_mm_x), int(pos_mm_y), color_string
+
+#save image with marker at selected ball
+def save_location_image(circles, filename, location_filename):
+    circle = circles[0][0]
+    src = cv2.imread(cv2.samples.findFile(filename), cv2.IMREAD_COLOR)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        return -1
+    
+    center = (int(circle[0]), int(circle[1]))
+    radius = int(circle[2])
+    img = cv2.circle(src, center, radius, (255, 0, 255), 3)
+    cv2.imwrite(location_filename, img)
